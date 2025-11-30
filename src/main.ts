8dbdac98f5a94c942642879f5aa41df7b1967907
Dickson Daniel Peprah <techpepson@gmail.com>
2025-11-30 22:16:51 +0000

    feat(console sever):develop the services and controllers to update the console content

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,26 @@ async function bootstrap() {
   configDotenv();
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.setGlobalPrefix('api');
+
+  app.enableCors({
+    origin: [
+      'https://inclusive-console.sbuildsolutions.org',
+      'http://localhost:3000',
+      'http://localhost:5174',
+      'http://localhost:5173',
+      'http://localhost:5175',
+    ],
+    allowedHeaders: [
+      'Content-Type',
+      'Authorization',
+      'Accept',
+      'Origin',
+      'X-Requested-With',
+      'Access-Control-Allow-Origin',
+    ],
+    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE', 'OPTIONS'],
+    credentials: true,
+  });
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
